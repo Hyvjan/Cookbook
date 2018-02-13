49e97a2743e9dc147433a9e48f4192658abb0e76
janne <janne.j.hyvonen@gmail.com>
2018-02-13 22:08:05 +0200

    AddRecipe now asks recipe name, ingredient name and amount and adds them to state. Next need to add functionality to push the new recipe to Layout.js state so it is shown.

--- static/src/components/AddRecipe/AddRecipe.js
+++ static/src/components/AddRecipe/AddRecipe.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './AddRecipe.css';
 
-const addIngredient = (props) => {
+class AddIngredient extends Component {
+
+  state = {
+    'ingredients': {},
+    'ingredientToAdd': "",
+    'amountToAdd': "",
+    'currentRecipeName':""
+  }
+
+  currentIngredientHandler = (evt) => {
+    this.setState({'ingredientToAdd': evt.target.value })
+  }
+
+  currentAmountHandler = (evt) => {
+    this.setState({'amountToAdd': evt.target.value })
+  }
+
+  currentRecipeHandler = (evt) => {
+    this.setState({'currentRecipeName': evt.target.value })
+  }
+
+  addIngredient = () => {
+    const oldIngredients = {...this.state}
+    const ingredient = this.state.ingredientToAdd
+    const amount= this.state.amountToAdd
+    const recipe = this.state.currentRecipeName
+    const addJson = {}
+    addJson[ingredient]=amount
+    addJson['name']=recipe
+    //{ingredient : amount, 'name': recipe}
+    oldIngredients.ingredients[ingredient]=amount
+    oldIngredients.ingredients['name']=recipe
+    const newIngredients = oldIngredients.ingredients
+    this.setState({'ingredients':newIngredients, 'ingredientToAdd': "",
+    'amountToAdd': "",})
+    console.log(this.state)
+  }
+
+  render () {
+
   return (
   <div className={classes.Container}>
-    <input type="text" id="ingredient" />
-    <button>Save ingredient</button>
+
+    <input type="text" value={this.state.currentRecipe}
+      onChange={(evt) => this.currentRecipeHandler(evt)}
+      placeholder="Add recipe name"/>
+    <input type="text" value={this.state.ingredientToAdd}
+      onChange={(evt) => this.currentIngredientHandler(evt)}
+      placeholder="Add ingredient name"/>
+    <input type="text" value={this.state.amountToAdd}
+      onChange={(evt) => this.currentAmountHandler(evt)}
+      placeholder="Add ingredient amount"/>
+    <button onClick={this.addIngredient}>Save ingredient</button>
+    <button >Save recipe</button>
   </div>
 )
+}
 };
 
-export default addIngredient;
+export default AddIngredient;
